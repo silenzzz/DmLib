d8446aa62ed6bb7748823d0e7f6666bb5c15b154
Vadim Guseynov <hgt166782@gmail.com>
2020-07-27 23:31:06 +0300

    Change stuff

--- Window/Transparency.cs
+++ Window/Transparency.cs
@@ -15,20 +15,36 @@ namespace DmLib.Window
         [DllImport("user32.dll")]
         private static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
 
+        [DllImport("user32.dll")]
+        private static extern bool GetLayeredWindowAttributes(IntPtr hWnd,uint crKey, out byte bAlpha, uint dwFlags);
+
         private const int GWL_EXSTYLE = -20;
         private const int WS_EX_LAYERED = 0x80000;
         private const int LWA_ALPHA = 0x2;
 
         /// <summary>
-        /// Sets the window transparency
+        /// Sets the window transparency.
         /// </summary>
-        /// <param name="p"> Target process </param>
-        /// <param name="opacityPercent"> Transparency precent </param>
+        /// <param name="p"> Target process. </param>
+        /// <param name="opacityPercent"> Transparency precent. </param>
         public static void SetWindowTransparency(Process p, int opacityPercent)
         {
             SetWindowLong(p.MainWindowHandle, GWL_EXSTYLE,
                 GetWindowLong(p.MainWindowHandle, GWL_EXSTYLE) ^ WS_EX_LAYERED);
             SetLayeredWindowAttributes(p.MainWindowHandle, 0, (byte)((255 * opacityPercent) / 100), LWA_ALPHA);
+        }
+
+        /// <summary>
+        /// Gets the window transparency.
+        /// </summary>
+        /// <param name="p">The process.</param>
+        /// <returns> Opacity precent. </returns>
+        public static uint GetWindowTransparency(Process p)
+        {
+            //SetWindowLong(p.MainWindowHandle, GWL_EXSTYLE,
+                //GetWindowLong(p.MainWindowHandle, GWL_EXSTYLE) ^ WS_EX_LAYERED);
+            GetLayeredWindowAttributes(p.MainWindowHandle, 0, out byte alpha, 0);
+            return (byte)((255 * alpha) / 100);
         }
     }
 }
